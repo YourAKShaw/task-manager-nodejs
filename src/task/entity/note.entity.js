04a3e77bd3e4f989549560034727c987ce6f62bc
Ak-Shaw <ayushshawz@gmail.com>
2022-08-09 08:54:46 +0530

    feat(task): add Task and Note entities

---
+++ src/task/entity/note.entity.js
@@ -0,0 +1,18 @@
+/**
+ * Represents a task note.
+ * @typedef {Object} Note
+ */
+export default class Note {
+  title;
+  body;
+
+  /**
+   * @constructor
+   * @param {string} title - The title of the note.
+   * @param {string} body - The body of the note.
+   */
+  constructor(title, body) {
+    this.title = title;
+    this.body = body;
+  }
+}
---
+++ src/task/entity/task.entity.js
@@ -0,0 +1,59 @@
+/**
+ * sample object of type Task
+ * {
+ *   "id": 31,
+ *   "title": "I am a task!",
+ *   "description": "This is the task description",
+ *   "deadline": "dd/mm/yyyy",
+ *   "notes": [
+ *     {
+ *       "title": "random note title",
+ *       "body": "I am the note body"
+ *     },
+ *     {
+ *       "title": "random note title",
+ *       "body": "I am the note body"
+ *     },
+ *     {
+ *       "title": "random note title",
+ *       "body": "I am the note body"
+ *     }
+ *   ],
+ *   "completed": false
+ * }
+ */
+
+// eslint-disable-next-line no-unused-vars
+import Note from './note.entity';
+
+/**
+ * Represents a task.
+ * @typedef {Object} Task
+ */
+export default class Task {
+  id;
+  title;
+  description;
+  deadline;
+  notes;
+  completed;
+
+  /**
+   * Represents a book.
+   * @constructor
+   * @param {number} id - The id of the task.
+   * @param {string} title - The title of the task.
+   * @param {string} description - The description of the task.
+   * @param {string} deadline - The deadline of the task.
+   * @param {Note[]} notes - The notes of the task.
+   * @param {boolean} completed - Whether the task is completed or not.
+   */
+  constructor(id, title, description, deadline, notes, completed) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.deadline = deadline;
+    this.notes = notes;
+    this.completed = completed;
+  }
+}
