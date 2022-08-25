f3f92d1a1a4bface52eb7662eae8a1f089a865a6
Ak-Shaw <ayushshawz@gmail.com>
2022-08-25 08:31:37 +0530

    feat(log): update log structure and add emoji for info and error logs

--- src/common/logger.js
+++ src/common/logger.js
@@ -1,6 +1,7 @@
 /* eslint-disable indent */
 /* eslint-disable object-curly-spacing */
 const winston = require('winston');
+const emoji = require('node-emoji');
 
 const logger = (module.exports = winston.createLogger({
   transports: [new winston.transports.Console()],
@@ -10,11 +11,18 @@ const logger = (module.exports = winston.createLogger({
     winston.format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss',
     }),
-    winston.format.printf(
-      (info) =>
-        `${info.timestamp} ${info.level}: ${info.message}` +
-        (info.splat !== undefined ? `${info.splat}` : ' '),
-    ),
+    winston.format.printf((info) => {
+      let emojiToLog = '';
+      if (info.level === '\x1B[32minfo\x1B[39m') {
+        emojiToLog = emoji.get('white_check_mark');
+      } else if (info.level === '\x1B[31merror\x1B[39m') {
+        emojiToLog = emoji.get('x');
+      }
+      return (
+        `[${info.timestamp}] ${emojiToLog} [${info.level}]: ${info.message}` +
+        (info.splat !== undefined ? `${info.splat}` : ' ')
+      );
+    }),
   ),
 }));
 
